d5afd8eca372fa78962e0f6c8cdea73dbb1791f6
teerakanotk <teerakan.otk@gmail.com>
2025-03-07 22:11:41 +0700

    updated blade-skill

--- app/not-found.js
+++ app/not-found.js
@@ -1,9 +1,1 @@
-import { NotFoundPage } from "nextra-theme-docs";
-
-export default function NotFound() {
-  return (
-    <NotFoundPage content="Submit an issue" labels="broken-link">
-      <h1>The page is not found</h1>
-    </NotFoundPage>
-  );
-}
+export { NotFoundPage as default } from "nextra-theme-docs";
